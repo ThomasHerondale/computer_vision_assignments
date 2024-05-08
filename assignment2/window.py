9f65d7d3933da2ccf5d9d338416418bbd3ab9334
ciposs <federicocpl@outlook.it>
2024-05-08 12:54:48 +0200

    Conclusione scrittura del modulo window.py

--- assignment2/window.py
+++ assignment2/window.py
@@ -7,7 +7,7 @@ from validation import tune_hyperparameters
 from new import load_dataset
 
 
-def standardize_size(images: [np.ndarray], h: int = 700, w: int = 600) -> [(np.ndarray, float, float)]:
+def standardize_size(images: [np.ndarray], h: int = 700, w: int = 500) -> [(np.ndarray, float, float)]:
     standardized_images: [np.ndarray] = []
     if images is not None:
         for image in images:
@@ -15,11 +15,11 @@ def standardize_size(images: [np.ndarray], h: int = 700, w: int = 600) -> [(np.n
                 # Se l'immagine originaria è più grande rispetto alla dimensione (h, w) stabilite,
                 # applico il filotro di Gauss
                 image_h, image_w, _ = image.shape
-                scale_w: float = w/image_w
-                scale_h: float = h/image_h
+                scale_w: float = w / image_w
+                scale_h: float = h / image_h
                 if image_h > h or image_w > w:
                     filtered_image = cv2.GaussianBlur(image, (3, 3), 5)
-                    resized_image= cv2.resize(filtered_image, (w, h), interpolation=cv2.INTER_CUBIC)
+                    resized_image = cv2.resize(filtered_image, (w, h), interpolation=cv2.INTER_CUBIC)
                     standardized_images.append((resized_image, scale_h, scale_w))
                 else:
                     # se l'immagine è piccola rispetto a (h, w) allora faccio semplicemente la resize
@@ -32,7 +32,7 @@ def standardize_size(images: [np.ndarray], h: int = 700, w: int = 600) -> [(np.n
     return standardized_images
 
 
-def  sliding_window(image, window_size=(64, 128), stride: (int, int) = (20, 20)) -> (int, int, np.array):
+def sliding_window(image, window_size=(64, 128), stride: (int, int) = (20, 20)) -> (int, int, np.array):
     """
         Il generatore sliding window consente alla finestra di scorrimento di scorrere localmente sull'immagine
         :param image: immagine da analizzare
@@ -58,7 +58,6 @@ def resize_img(image: np.ndarray, scale: float = 1.0) -> np.ndarray:
 # immagine ridimensionata la invoco
 def gaussian_pyramid(image: np.ndarray, scale: float = 1.0, sigma: int = 5,
                      kernel_size: (int, int) = (3, 3)) -> np.ndarray:
-
     if scale < 1.0:
         filtered_image = cv2.GaussianBlur(image, ksize=kernel_size, sigmaX=sigma, sigmaY=sigma)
         resized_image = resize_img(filtered_image, scale)
@@ -69,14 +68,13 @@ def gaussian_pyramid(image: np.ndarray, scale: float = 1.0, sigma: int = 5,
         return image
 
 
-def show_window(image: (np.ndarray, float, float),
+def show_window(image: np.ndarray,
                 hog: cv2.HOGDescriptor,
                 clf: Pipeline,
                 scale: float = 1.0,
-                window_size: (int, int) = (64, 128)) -> (np.ndarray, (float, float), [(int, int, int, int, int)]):
-    if scale == 1.0:
-        key_image: np.ndarray = image # memorizzo l'immagine che utilizzero nel mio dizionario
-
+                window_size: (int, int) = (64, 128)
+                ) -> [(int, int, int, int, np.ndarray)]:
+    decisions: [()] = []
     image_scaled = gaussian_pyramid(image, scale=scale)
     for (x, y, window) in sliding_window(image_scaled, window_size):
         if window.shape[0] != window_size[1] or window.shape[1] != window_size[0]:
@@ -87,22 +85,45 @@ def show_window(image: (np.ndarray, float, float),
         descriptor = hog.compute(window)
         # Passo al classificatore
         f = clf.predict([descriptor])
-        if f == 1:
-            # memorizzo la tupla di cordinate della finestra (x, y, x2, y1)
+        #print(f"Classificatore: {f}\n-----------------")
+        if f[0] == 1:
+            # memorizzo la tupla di cordinate della finestra (x, y, x2, y2)
             # Chiamo la funzione decision_function per determinare il parametro
-            print("fai")
+            c = clf.decision_function([descriptor])
+            #print(f"Decision: {c}\n-----------------------")
+            #print(type(c))
+            decisions.append((x, y, x + window_size[0], y + window_size[1], c))
+            #print(f"Array: {decisions}\n")
         cv2.imshow("Sliding Window", clone_image)
         cv2.waitKey(1)
-        #time.sleep(0.15)
+        #time.sleep(0.50)
+    return decisions
 
 
-def multiscale_function(images: [(np.ndarray, float, float)]):
+"""x = [
+1.    (1.2, 0.6),
+2.    [
+   0.  (0, 0, 64, 128, [0.78]),
+   1.  (0, 0, 64, 128, [0.78]),
+   2.  (0, 0, 64, 128, [0.78]),
+   3.  (0, 0, 64, 128, [0.78])
+     ]
+]
+"""
+
+
+def multiscale_function(images: [(np.ndarray, float, float)]) -> [(float, float), [(int, int, int, int, np.ndarray)]]:
     x, y = load_dataset(size=1_000)
     clf, _, _ = tune_hyperparameters(x, y)
     hog = cv2.HOGDescriptor()
+    list_of_return: [(), [()]] = []
     for (image, scale_x, scale_y) in images:
+        plausibile_rectangular_regions: [()] = []
         for scale in (1.3, 1.0, 0.5):
-            show_window((image, scale_x, scale_y), hog, clf, scale)
+            plausibile_rectangular_regions += show_window(image, hog, clf, scale)
+        list_of_return += [(scale_x, scale_y), plausibile_rectangular_regions]
+        print(f"list_of_return: {list_of_return}\n")
+    return list_of_return
 
 
 # ------------------------------------------------------------------------------------------------------------------
@@ -142,8 +163,9 @@ def read(cartella: str, numero_immagini=1_000):
 image_path = "/Users/fede/Desktop/Unipa/anno4/Visione artificiale/Assignment/2/WiderPerson/Images"
 immagini = read(image_path)
 immagini_2 = standardize_size(immagini)
-for (image, i, j) in immagini_2:
+"""for (image, i, j) in immagini_2:
     cv2.imshow("Prova", image)
     cv2.waitKey(0)
     print(f"Scale_h: {i}\nScale_w: {j}\n-------------------")
-#multiscale_function(immagini_2)
+#"""
+multiscale_function(immagini_2)
