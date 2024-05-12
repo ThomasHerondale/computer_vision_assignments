3b6e3c1ea8b1ebce97f26be60da902a1b7851666
antoninocentonze <anto.02@outlook.it>
2024-05-12 17:45:52 +0200

    ora funziona, anche se di merda

--- assignment2/main.py
+++ assignment2/main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     x, y = data.load_dataset(size=0.5)
     model,_,_ = validation.tune_hyperparameters(x,y)
     immagini, bboxes = test.load_test_set()
-    i = 1
+    i = 10
     immagini, bboxes = immagini[:i], bboxes[:i]
     data = window.predict(images=immagini, clf=model)
     test.test_model(immagini, bboxes, data)
