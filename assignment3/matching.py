88d648573aed8c8490afb0522c1032eceae2556e
ciposs <federicocpl@outlook.it>
2024-06-12 10:31:54 +0200

    correzioni

--- assignment3/matching.py
+++ assignment3/matching.py
@@ -12,9 +12,9 @@ def compute_euclidian_distance(detection_point, tracking_point) -> float:
     """
     Function to compute the cost of linking two points. This cost is calculated as euclidian distance
     between the two points.
-    :param: detection_point: list of [x_c, y_c, area, aspect_ratio] of the frame in t-1
-    :param: tracking_point: list of [x_c, y_c, area, aspect_ratio] of the frame in t
-    :return: d: is cost of linking two points
+    :param detection_point: list of [x_c, y_c, area, aspect_ratio] of the frame in t-1
+    :param tracking_point: list of [x_c, y_c, area, aspect_ratio] of the frame in t
+    :return d: is cost of linking two points
     """
     d_x = tracking_point[0] - detection_point[0]
     d_y = tracking_point[1] - detection_point[1]
@@ -30,8 +30,8 @@ def _convert_bbox(bbox) -> np.ndarray:
     :return numpy array [x, y, w, h]
     """
     x_c, y_c, w, h = bbox
-    x = float(x_c - w / 2) # forse è meglio convertire tutto a intero ?
-    y = float(y_c - h / 2)
+    x = int(x_c - w / 2) # forse è meglio convertire tutto a intero ?
+    y = int(y_c - h / 2)
     return np.array([x, y, w, h], dtype=float)
 
 
@@ -42,8 +42,8 @@ def _compute_descriptor_hog(frame, bbox_converted: np.ndarray):
     :param bbox_converted: a simple np array in form [x, y, w, h]
     :return features: features of cropped image that include pedestrian
     """
-    x1, y1, w, h = bbox_converted # estraggo gli elementi di una bbox
-    cropped_image = frame[y1:(y1 + h), x1:(x1 + h)] # ritaglio l'imagine
+    x1, y1, w, h = bbox_converted  # estraggo gli elementi di una bbox
+    cropped_image = frame[y1:(y1 + h), x1:(x1 + h)]  # ritaglio l'imagine
 
     # Calcola l'HOG dell'immagine ritagliata
     features, hog_image = hog(cropped_image, orientations=9, pixels_per_cell=(8, 8),
