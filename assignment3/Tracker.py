a2f951bcd5826ff136c2b6cb2232ccd27ac0fd43
ciposs <federicocpl@outlook.it>
2024-06-12 13:04:01 +0200

    correzioni

--- assignment3/Tracker.py
+++ assignment3/Tracker.py
@@ -1,7 +1,10 @@
+import cv2
 import numpy as np
 from skimage.feature import hog
 from skimage import exposure
 import random as rnd
+from matching import _convert_bbox
+
 
 class Tracker:
     def __init__(self, bbox, conf_score, label, img):
@@ -17,7 +20,7 @@ class Tracker:
         self.bbox = bbox
         self.conf_score = conf_score
         self.bbox_difference = np.array([0, 0, 0, 0], dtype=np.float32)
-        self.descriptor = self.compute_descriptor(img, bbox)
+        self.descriptor = self.compute_descriptor_hog(img, bbox)
         self.color = rnd.choice(self.__COLORS)
 
     @classmethod
@@ -49,22 +52,25 @@ class Tracker:
         self.counter_last_update = 0
 
     @staticmethod
-    def compute_descriptor_hog(img, bbox):
+    def compute_descriptor_hog(frame, bbox):
         """
-        Compute the HOG descriptor of the object
+        Compute the HOG descriptor of the object.
+        :param frame: current frame of image
+        :param bbox_converted: a simple np array in form [x_l, y_t, w, h]
+        :return features: features of cropped image that include pedestrian
         """
-        x1, y1, x2, y2 = bbox.astype(int)
-        cropped_image = img[y1:y2, x1:x2]
-
+        bbox_converted = _convert_bbox(bbox=bbox)
+        x_l, y_t, w, h = bbox_converted  #estraggo gli elementi di una bbox
+        cropped_image = frame[y_t:(y_t + h), x_l:(x_l + w)]  # ritaglio l'imagine
+        resize = cv2.resize(cropped_image, dsize=(64, 128))
         # Calcola l'HOG dell'immagine ritagliata
-        features, hog_image = hog(cropped_image, orientations=9, pixels_per_cell=(8, 8),
-                                  cells_per_block=(2, 2), visualize=True, multichannel=True)
+        features, hog_image = hog(resize, orientations=9, pixels_per_cell=(8, 8),
+                                  cells_per_block=(2, 2), visualize=True, channel_axis=2)
 
         # Normalizzo l'HOG
         features = exposure.rescale_intensity(features, in_range=(0, 10))
 
         return features
-
 
     @property
     def current_position(self):
--- assignment3/matching.py
+++ assignment3/matching.py
@@ -1,4 +1,6 @@
 import math
+
+import cv2
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 from skimage.feature import hog
@@ -8,7 +10,7 @@ from detection import get_detections
 
 
 #  the first version
-def compute_euclidian_distance(detection_point, tracking_point) -> float:
+def _compute_euclidian_distance(detection_point, tracking_point) -> float:
     """
     Function to compute the cost of linking two points. This cost is calculated as euclidian distance
     between the two points.
@@ -25,29 +27,30 @@ def compute_euclidian_distance(detection_point, tracking_point) -> float:
 
 def _convert_bbox(bbox) -> np.ndarray:
     """
-    convert_bbox coverte a bbox from form [x_c, y_c, w, h] into numpy array [x, y, w, h]
+    convert_bbox coverte a bbox from form [x_c, y_c, w, h] into numpy array [x_l, y_t, w, h]
     :param bbox: list of [x_c, y_c, w, h]
-    :return numpy array [x, y, w, h]
+    :return numpy array [x_, y, w, h]
     """
     x_c, y_c, w, h = bbox
     x = int(x_c - w / 2) # forse è meglio convertire tutto a intero ?
     y = int(y_c - h / 2)
-    return np.array([x, y, w, h], dtype=float)
+    return np.array([x, y, w, h], dtype=int)
 
 
-def _compute_descriptor_hog(frame, bbox_converted: np.ndarray):
+def _compute_descriptor_hog(frame, bbox):
     """
     Compute the HOG descriptor of the object.
     :param frame: current frame of image
-    :param bbox_converted: a simple np array in form [x, y, w, h]
+    :param bbox: a simple array in form [x, y, w, h]
     :return features: features of cropped image that include pedestrian
     """
+    bbox_converted = _convert_bbox(bbox)
     x1, y1, w, h = bbox_converted  # estraggo gli elementi di una bbox
-    cropped_image = frame[y1:(y1 + h), x1:(x1 + h)]  # ritaglio l'imagine
-
+    cropped_image = frame[y1:(y1 + h), x1:(x1 + w)]  # ritaglio l'imagine
+    resize = cv2.resize(cropped_image, dsize=(64, 128))
     # Calcola l'HOG dell'immagine ritagliata
-    features, hog_image = hog(cropped_image, orientations=9, pixels_per_cell=(8, 8),
-                              cells_per_block=(2, 2), visualize=True, multichannel=True)
+    features, hog_image = hog(resize, orientations=9, pixels_per_cell=(8, 8),
+                              cells_per_block=(3, 3), visualize=True, channel_axis=2)
 
     # Normalizzo l'HOG
     features = exposure.rescale_intensity(features, in_range=(0, 10))
@@ -55,9 +58,9 @@ def _compute_descriptor_hog(frame, bbox_converted: np.ndarray):
     return features
 
 
-def _compute_cosine_similarrity(a, b):
+def _compute_cosine_similarity(a, b):
     """
-    _compute_cosine_similarrity calculate the cosine of angle alpha of two vector, a and b, and then it subtract to one
+    _compute_cosine_similarity calculate the cosine of angle alpha of two vector, a and b, and then it subtract to one
     :param a: bbox's features of current frame
     :param b: bbox's features of previous frame
     """
@@ -65,28 +68,52 @@ def _compute_cosine_similarrity(a, b):
     return 1 - cosine
 
 
-def calculate_cost_matrix(detection_list, track_list) -> np.ndarray:
+def _calculate_cost_matrix(detections_list, tracks_list, detections_features, lambda_1, lambda_2) -> np.ndarray:
+    """
+    Compute the cost matrix using euclidian distance and cosine similarity.
+    :param detections_list: detections list in the form of [x_c, y_c, width, height]
+    :param tracks_list: list of tracks in the form of [x_c, y_c, width, height]
+    :param detections_features: list of  HOG descriptors of current frame
+    :param lambda_1: first hyperparameter
+    :param lambda_2: second hyperparameter
+    :return: the cost matrix
+    """
+    # creo la matrice di soli zero di len(tracks_list) * len(detections_list)
+    cost_matrix = np.zeros((len(tracks_list), len(detections_list)), np.float32)
 
-    cost_matrix = np.zeros((len(track_list), len(detection_list)), np.float32)
+    # estraggo Hog descriptor dei trackers
+    tracks_features = [track.descriptor for track in tracks_list]
 
-    for trak_i, track in enumerate(track_list):
-        for det_i, detection in enumerate(detection_list):
-            cost = compute_euclidian_distance(detection, track)
+    # costruisco la matrice dei costi
+    for trak_i, track in enumerate(tracks_list):
+        for det_i, detection in enumerate(detections_list):
+            cost = (lambda_1 * _compute_euclidian_distance(detection, track) +
+                    lambda_2 * _compute_cosine_similarity(detections_features, tracks_features))
             cost_matrix[trak_i, det_i] = cost
     return cost_matrix
 
 
-def matching(img, detections, tracks, threshold: int):
+def matching(current_frame, detections, tracks,lambda_1: float, lambda_2: float, threshold: int):
     """
-    :param tracks: lista di oggetti alla quale è gia stato assegnato un id
+    This function apply Hungarian algorithm to solve the assignment problem
+    :param current_frame: current frame of video
+    :param detections: detections list in the form of [x_c, y_c, width, height]
+    :param tracks: list of tracks in the form of [x_c, y_c, width, height]
+    :param lambda_1: first hyperparameter
+    :param lambda_2: second hyperparameter
+    :param threshold:
     """
+
     track_ind: list[int] = [i for i in range(len(tracks))]
     detect_ind: list[int] = [i for i in range(len(detections))]
 
     if len(detect_ind) == 0 or len(track_ind) == 0:
         return [], track_ind, detect_ind
 
-    cost_matrix = calculate_cost_matrix(detections, tracks)
+    # creo la lista di fetures delle detection
+    detections_features = [_compute_descriptor_hog(current_frame, detection) for detection in detections]
+
+    cost_matrix = _calculate_cost_matrix(detections, tracks, detections_features, lambda_1, lambda_2)
 
     row_ind, col_ind = linear_sum_assignment(cost_matrix)
 
